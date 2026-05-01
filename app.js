a79a97709dd02b874776792275a0b87801e1b96a
Mansur550 <mansurislam59@gmail.com>
2026-05-02 01:30:30 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let a = "mansur";
+a = "mansur Islam";
+console.log(a);
+
+const country = "Bangladesh";
+// country = "India"; // This will cause an error because 'country' is a constant
+console.log(country);
+
+//if else statement
+let age = 20;
+if (age >= 18 && age < 30) {
+    console.log("You are in your 20s");
+} else {
+    console.log("You are a minor.");
+}
