c827e51d8e3726b1677006ca49319e770d138329
Mansur550 <mansurislam59@gmail.com>
2026-05-04 01:57:04 +0600

    Updated

---
+++ array-methods.js
@@ -0,0 +1,6 @@
+const products = [
+    { name: "redmi", brand: "Xaiomi", price : 15000 },
+    { name: "samsung", brand: "Samsung", price : 20000 },
+    { name: "realme", brand: "Realme", price : 18000 },
+    { name: "iphone", brand: "Apple", price : 50000 },
+];
