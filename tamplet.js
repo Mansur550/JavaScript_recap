8ce61519e09e8831bbbbab6c0a91e83737969bdf
Mansur550 <mansurislam59@gmail.com>
2026-05-04 01:00:47 +0600

    Arrow function

--- tamplet.js
+++ tamplet.js
@@ -7,3 +7,13 @@ const fullName = `${fName} ${lName}`; //template literal
 console.log(fullName);
 
 console.log(`I am ${fName} ${lName}. I am learning JavaScript.`);
+
+
+// Arrow Function
+const sum = (a, b) => a + b;
+console.log(sum(5, 10));    
+// Arrow function with multiple lines
+const sumWithMultipleLines = (a, b) => {
+    const result = a + b;
+    return result;
+}   
