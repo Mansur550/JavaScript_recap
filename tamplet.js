e34d2e64d93c7d12443e6865cd65b7328b0185a2
Mansur550 <mansurislam59@gmail.com>
2026-05-04 00:54:33 +0600

    tamplete string `

---
+++ tamplet.js
@@ -0,0 +1,9 @@
+//tamplet String
+const fName = "Mansur";
+const lName = "Islam";
+
+// const fullName = fName + " " + lName; //concatenation
+const fullName = `${fName} ${lName}`; //template literal
+console.log(fullName);
+
+console.log(`I am ${fName} ${lName}. I am learning JavaScript.`);
