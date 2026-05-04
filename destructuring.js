74500fa6d30c5d570ba658cb4e2a820beece3c86
Mansur550 <mansurislam59@gmail.com>
2026-05-05 01:53:34 +0600

    Object  destructring

--- destructuring.js
+++ destructuring.js
@@ -7,3 +7,15 @@ console.log(firstFriend);
 console.log(secondFriend);
 console.log(otherFriends); //rest operator to collect the remaining elements of the array
 
+// Object destructuring     
+const person = {
+    name: "Mansur Islam",
+    age: 25,
+    city: "Dhaka",
+    country: "Bangladesh"
+}; 
+
+const { name, age, ...otherDetails } = person;  
+console.log(name);
+console.log(age);
+console.log(otherDetails); //rest operator to collect the remaining properties of the object      
