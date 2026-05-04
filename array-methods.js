5b0f676a486259615bfec0540dcdb41b6b6c3739
Mansur550 <mansurislam59@gmail.com>
2026-05-05 01:37:37 +0600

    U

--- array-methods.js
+++ array-methods.js
@@ -4,3 +4,14 @@ const products = [
     { name: "realme", brand: "Realme", price : 18000 },
     { name: "iphone", brand: "Apple", price : 50000 },
 ];
+
+// map method
+const productNames = products.map(product => product.name);
+console.log(productNames);
+
+//forEach method
+products.forEach(product => console.log(product.name));
+
+//filter method
+const result = products.filter(product => product.price > 15000);
+console.log(result);
